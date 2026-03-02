a2a797f1abd059723de52b387c4165cbce016ae5
Gospellinx <gospelocheni@gmail.com>
2026-03-02 10:23:48 +0100

    navbar mobile setup 2

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const MobileNav = () => {
   return (
     <div>
-      Mobile Nav
+      {/* Overlay */}
+      <div className='fixed top-0 transform transition-all duration-500 z-[10000] left-0 right-0 buttom-0 bg-black opacity-70 w-full h-[100vh]' />
     </div>
   )
 }
