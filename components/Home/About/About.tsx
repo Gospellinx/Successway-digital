77a43610c8032573a5ee18f51f8ba56080d393e3
Gospellinx <gospelocheni@gmail.com>
2026-03-03 15:30:23 +0100

    About Section setup

---
+++ components/Home/About/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='pt-16 pb-16 '>
+      {/* Two sections in grid */}
+      <div className="w-full mx-auto grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
+        
+      </div>
+    </div>
+  )
+}
+
+export default About
