91feca98834070a97053f93b349fc40f06583846
Gospellinx <gospelocheni@gmail.com>
2026-03-04 11:53:36 +0100

    Data Setup

---
+++ components/Home/Course/CourseCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// define the type
+type Props = {
+  course: {
+    id: number;
+    image: string;
+    title: string;
+    price: number;
+    author: string;
+    reviewNumber: number;
+    lessons: number;
+    students: number;
+    category: string;
+  };
+};
+
+const CourseCard = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default CourseCard
--- components/Home/Course/Courses.tsx
+++ components/Home/Course/Courses.tsx
@@ -1,10 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
+import { coursesData } from '../../../data/data'
+import CourseCard from './CourseCard'
 
 const Courses = () => {
   return (
     <div className='pt-16 pb-12 relative bg-gray-200 '>
+      {/* Bounce Ball */}
       <Image src="/images/cb.png" alt="Course Banner" width={800} height={800} className='absolute top-[30%] animate-bounce'/>
+      <div className='w-[90%] mx-auto'>
+        <h1 className='text-4xl md:text-5xl font-bold text-black'>Our Programs / Services </h1>
+        <div className='md:mt-16 mt-10 gird grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10'>
+          {coursesData.map((course)=> {
+            return <div key={course.id}>
+              <CourseCard course={course}/>
+            </div>
+          })}
+        </div>
+      </div>
     </div>
   )
 }
