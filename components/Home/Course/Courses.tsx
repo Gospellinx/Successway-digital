9157aa6f52882ae615d7f8fa7f2005906acdd4ea
Gospellinx <gospelocheni@gmail.com>
2026-03-04 10:08:46 +0100

    Courses Section Start

--- components/Home/Course/Courses.tsx
+++ components/Home/Course/Courses.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Courses = () => {
   return (
     <div className='pt-16 pb-12 relative bg-gray-200 '>
-      <Image src="/public/images/cb.png" alt="Course Banner" width={800} height={800}/>
+      <Image src="/images/cb.png" alt="Course Banner" width={800} height={800} className='absolute top-[30%] animate-bounce'/>
     </div>
   )
 }
