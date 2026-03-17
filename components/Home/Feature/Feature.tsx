4a92102346105834634b75a81a0799724fa8b02b
Gospellinx <gospelocheni@gmail.com>
2026-03-17 17:09:01 +0100

    Feature Section setup

---
+++ components/Home/Feature/Feature.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from 'next/image'
+import React from 'react'
+import Tilt from 'react-parallax-tilt'
+
+const Feature = () => {
+  return (
+    <div className='pt-16 pb-16'>
+      <div className='mt-8 grid grid-cols-1 xl:grid-cols-2 items-center gap-12 w-[90%] mx-auto'>
+        {/* Image */}
+        <Tilt>
+            <div>
+                <Image src="/images/f.png" alt='image' width={1000} height={1000} />
+            </div>
+        </Tilt>
+        <div>text</div>
+      </div>
+    </div>
+  )
+}
+
+export default Feature
