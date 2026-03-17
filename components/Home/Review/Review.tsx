d9ae950ca4ec11f0221cba046cb40ba8a9ff89d2
Gospellinx <gospelocheni@gmail.com>
2026-03-17 19:21:59 +0100

    Review Section set up

---
+++ components/Home/Review/Review.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Review = () => {
+  return (
+    <div className='pt-20 pb-16 bg-black'>
+      <div className='w-[90%] mx-auto grid grid-cols-1 xl:grid-cols-3 items-center gap-20'>
+        {/* Text Content */}
+        <div className='xl:col-span-1 mt-6 '>
+            <div></div>
+        </div>
+        {/* Slider */}
+        <div className='xl:col-span-2'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Review
